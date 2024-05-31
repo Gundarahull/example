8ebbb01c563811e86020a120be8efbd118226e09
Gundarahull <r161731@rguktrkv.ac.in>
2024-05-31 10:42:32 +0530

    DB Connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const sequelize = require("./database/db");
+const app = express();
 
 //for body-parsing
-app.use(express.json())
-app.use(express.urlencoded(true))
+app.use(express.json());
+app.use(express.urlencoded(true));
 
-app.listen(8000,()=>{
-    console.log("Server is Started");
+sequelize.authenticate().then(() => {
+  console.log("Connection Done with DB");
+}).catch(()=>{
+    console.log("Connection Failed With DB");
 })
+sequelize.sync().then(() => {
+  console.log("Table Created");
+}).catch(()=>{
+    console.log(" Failed While Creating the Tables");
+})
+
+
+
+app.listen(8000, () => {
+  console.log("Server is Started");
+});
